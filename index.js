27fa472c46d9d2a5fcb03202913f304d24ae23fb
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-09-05 08:23:34 +0300

    added variables

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 var CC, YY, MM, DD, d, dayValue;
+var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var female = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 var Day = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
