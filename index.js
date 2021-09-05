55b7289e2961fda40b850def3d187f740d3ec04f
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-09-05 08:21:04 +0300

    edited variables

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 var CC, YY, MM, DD, d, dayValue;
-var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
-0
+var Day = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
